1a442f6f8f2580b3a501616b00cd2ba25daace86
Sebastian Correa <scorrea92@gmail.com>
2017-11-08 13:55:17 +0100

    Agrego los elementos del primer view, tambien conecto las action de los botones y los oulets necesarios

--- PitchPerfect2/ViewController.swift
+++ PitchPerfect2/ViewController.swift
@@ -10,16 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordingLabel: UILabel!
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var stopRecordingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        stopRecordingButton.isEnabled = false
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(<#T##animated: Bool##Bool#>)
+        print("viewWillAppear called")
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func recordAudio(_ sender: Any) {
+        print("record button was pressed")
+        recordingLabel.text = "Recording in Process"
+        stopRecordingButton.isEnabled = true
+        recordButton.isEnabled = false
+    }
+    @IBAction func stopRecording(_ sender: Any) {
+        print("StopRecord button was pressed")
+        stopRecordingButton.isEnabled = false
+        recordButton.isEnabled = true
+    }
+    
 }
 
