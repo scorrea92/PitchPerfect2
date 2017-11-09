48c36a7d8165fa61539eceadf828c57efa4c7789
Sebastian Correa <scorrea92@gmail.com>
2017-11-09 18:30:24 +0100

    Ordeno y configuro los botones que daran efecto a el audio.

--- PitchPerfect2/PlaySoundsViewController.swift
+++ PitchPerfect2/PlaySoundsViewController.swift
@@ -7,16 +7,45 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlaySoundsViewController: UIViewController {
-    var recordedAudioURL: URL!
+
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+    var recordedAudioURL:URL!
+    
+    var audioFile:AVAudioFile!
+    var audioEngine:AVAudioEngine!
+    var audioPlayerNode: AVAudioPlayerNode!
+    var stopTimer: Timer!
+    
+    enum ButtonType: Int {
+        case slow = 0, fast, chipmunk, vader, echo, reverb
+    }
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(recordedAudioURL)
-        // Do any additional setup after loading the view.
+        setupAudio()
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
